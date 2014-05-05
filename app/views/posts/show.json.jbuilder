70fdd495f840ea7efbfd76719f3ca0f82de98fb0
Carlos García de la Barrera <carlos.garcia@lextrend.net>
2014-05-05 16:34:55 +0200

    Inicial

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :author, :body, :published, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoPi::Application.config.secret_key_base = '49835200b8384e7cb592bac422699ed925c1e8efd77866b983344a4c08bbad14efd0da92467acbd0153a86c88166bb7a523161787d10dd079f9be1c81de06fd6'
